fab019337b16f2340a606f3d4ac06356f99b4103
mandro <androjaromi78@gmail.com>
2023-03-07 22:10:10 -0300

    wii logré que funcionara el consultar saldo y un array de objects con usuarios,datos  y pagina correspondiente

--- nuevoMandroEstado/main.js
+++ nuevoMandroEstado/main.js
@@ -1,27 +1,31 @@
+console.log("hola")
+
+const users = [
+    {nombre: "Andro", saldo: 770 , contraseña: "1", pag:"http://127.0.0.1:5500/nuevoMandroEstado/index2.html"},
+    {nombre: "Manzana", saldo: 340, contraseña: "12", pag:"http://127.0.0.1:5500/nuevoMandroEstado/index3.html"},
+    {nombre: "Vergil", saldo: 203 , contraseña: "123", pag: "http://127.0.0.1:5500/nuevoMandroEstado/index4.html"},
+]
 const btn = document.getElementById("btn")
 
+console.log("holiiii")
+
 btn.addEventListener("click", (event)=>{
+    console.log("holiuwuwuw")
+
     event.preventDefault()
+
     let user, password
     user = document.getElementById("user").value
     password = document.getElementById("password").value
     
-    if(user == "Mandro123" && password == "1234"){
-        window.location="index2.html";
-        alert("iniciaste sesión")
-    }else{
-        alert("tamal")
-    }
-    if(user == "ManzanaPro" && password == "1234"){
-        window.location="index2.html";
-        alert("iniciaste sesión")
-    }else{
-        alert("tamal")
-    }
-    if(user == "xXLautaroXx" && password == "1234"){
-        window.location="index2.html";
-        alert("iniciaste sesión")
-    }else{
-        alert("tamal")
+    console.log("hola buenas")
+
+    for(i = 0; i <  users.length; i++ ){
+        console.log("entrando al for")
+       
+        if(user == users[i].nombre && password == users[i].contraseña){
+            alert("Iniciaste Sesión!")
+            window.location=users[i].pag
+        }
     }
 })
--- nuevoMandroEstado/main2.js
+++ nuevoMandroEstado/main2.js
@@ -1,6 +1,34 @@
-const btn = document.getElementById("btn2")
+console.log("hola")
+const users = [
+    {nombre: "Andro", saldo: 770 , contraseña: "1"},
+    {nombre: "Manzana", saldo: 340, contraseña: "12"},
+    {nombre: "Vergil", saldo: 203 , contraseña: "123"},
+]
+const btn2 = document.getElementById("btn2")
+const btn3 = document.getElementById("btn3")
+const btn4 = document.getElementById("btn4")
+const btn5 = document.getElementById("btn5")
+const infosa = document.getElementById("info-sa")
 
 btn2.addEventListener("click", (event)=>{
     event.preventDefault()
     window.location = "index.html"
+})
+btn3.addEventListener("click", (event)=>{
+    event.preventDefault()
+    let pass = prompt("Ingrese su contraseña por favor");
+    for(i = 0; i < users.length; i++){
+        if(pass == users[i].contraseña){
+            console.log(users[i].saldo)
+            infosa.innerHTML = infosa.innerHTML + `<p> Su saldo es ${users[i].saldo}</p>`;
+        }
+    }
+})
+btn4.addEventListener("click", (event)=>{
+    event.preventDefault()
+    
+})
+btn5.addEventListener("click", (event)=>{
+    event.preventDefault()
+    
 })
